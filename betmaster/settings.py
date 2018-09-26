295cfcd57be390f2ca810ef11e3b6f7b427b0209
maddrum <maddrum9@gmail.com>
2018-09-26 21:48:10 +0300

    added some models

--- betmaster/settings.py
+++ betmaster/settings.py
@@ -29,13 +29,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'bets',
 ]
 
@@ -114,3 +114,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'bet.User'
--- bets/models.py
+++ bets/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# match status choices
+choices = (('Home Team Wins', 'home'), ('Guest Team Wins', 'away'), ('Tie', 'tie'))
+user_model = get_user_model()
+
+
+class User(AbstractUser):
+    """adds extra fields for user"""
+    bio = models.TextField()
+
+
+class Match(models.Model):
+    """saves data for match"""
+    home_team = models.CharField(max_length=255)
+    away_team = models.CharField(max_length=255)
+    match_start_time = models.DateField()
+    match_ended = models.BooleanField(default=False)
+    match_status = models.CharField(choices=choices)
+    score_home = models.IntegerField(default=0)
+    score_away = models.IntegerField(default=0)
+    created_on = models.DateField(default=timezone.now)
+    edited_on = models.DateField(auto_now=True)
+
+
+class UserPredictions(models.Model):
+    """save user prediction for users"""
+    user = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    predicted_match_state = models.CharField(choices=choices)
+    predicted_goals_home = models.IntegerField(default=0)
+    predicted_goals_away = models.IntegerField(default=0)
+    gained_points = models.IntegerField(default=0)
+    created_on = models.DateTimeField(default=timezone.now)
+    edited_on = models.DateTimeField(auto_now=True)
+
+
+class RankList(models.Model):
+    """Save and update current ranklist"""
+    user = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def update_ranklist(self, instance):
+        """Updates and save current playlist"""
+        pass
+
+
+class MatchComments(models.Model):
+    """saves comments about match"""
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    user = models.ForeignKey(user_model)
+    comment = models.TextField()
+
+
+class RankListComments(models.Model):
+    """saves comments about RankList"""
+    user = models.ForeignKey(user_model)
+    comment = models.TextField()
