98e8d11f5fd6aa15f07d7c359514b3a7d4fb269f
maddrum <maddrum9@gmail.com>
2018-10-10 21:58:26 +0300

    added pagination

--- betmaster/settings.py
+++ betmaster/settings.py
@@ -128,5 +128,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
 }
