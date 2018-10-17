89cfe1d602d801de1e44640de404849cc33c4ba5
maddrum <maddrum9@gmail.com>
2018-10-17 19:52:30 +0300

    added rest framework schema

--- bets/urls.py
+++ bets/urls.py
@@ -3,6 +3,7 @@ from bets import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.schemas import get_schema_view
 
 router = DefaultRouter()
 router.register('register', views.RegisterUser, base_name='register')
@@ -14,12 +15,14 @@ router.register('user_predictions', views.UserPredictionsView, base_name='user_p
 router.register('user_notes', views.UserPrivateNotesView, base_name='user_private_notes')
 
 app_name = 'bets'
-# TODO schemas
+# rest framework schema
+rest_schema_view = get_schema_view(title="betmaster API - REST FRAMEWORK SCHEMA")
 #  swagger
-schema_view = get_swagger_view(title='betmaster API')
+schema_view = get_swagger_view(title='betmaster API SWAGGER SCHEMA')
 urlpatterns = [
     url(r'', include(router.urls)),
     url(r'login_json', obtain_auth_token),
     url(r'schema/$', schema_view),
+    url(r'schema-rest/$', rest_schema_view)
 ]
 
